480287960fb83fef9950bc8eb50370e0121398b7
jeffQOSergio <167209680+jeffQOSergio@users.noreply.github.com>
2026-05-04 01:44:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -69,11 +69,11 @@ app.listen(PORT, () => {
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   ssl: {
     rejectUnauthorized: false
   }
@@ -81,8 +81,8 @@ const db = mysql.createConnection({
 
 db.connect(err => {
   if (err) {
-    console.log("Error DB:", err);
+    console.log("❌ Error DB:", err);
   } else {
-    console.log("Conectado a MySQL 🚀");
+    console.log("✅ Conectado a MySQL 🚀");
   }
 });
