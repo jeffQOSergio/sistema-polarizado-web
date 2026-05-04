05d8a0f7a0aa47ed454d94d7b9bd8debb60c7216
jeffQOSergio <167209680+jeffQOSergio@users.noreply.github.com>
2026-05-04 01:13:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -67,3 +67,20 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+db.connect(err => {
+  if (err) {
+    console.log("Error DB:", err);
+  } else {
+    console.log("Conectado a MySQL 🚀");
+  }
+});
