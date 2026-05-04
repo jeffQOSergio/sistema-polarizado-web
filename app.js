d7f68e40ff979fd7263ad55dad8aba6fdde3e494
jeffQOSergio <167209680+jeffQOSergio@users.noreply.github.com>
2026-05-04 00:52:10 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const URL = "https://TU-LINK-RENDER.onrender.com";
+
+async function login() {
+  const usuario = document.getElementById("usuario").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    const res = await fetch(URL + "/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ usuario, password })
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      document.getElementById("mensaje").style.color = "green";
+      document.getElementById("mensaje").innerText = "✔ Login correcto";
+
+      // Aquí luego puedes redirigir
+      // window.location.href = "dashboard.html";
+    } else {
+      document.getElementById("mensaje").innerText = data.message;
+    }
+
+  } catch (error) {
+    document.getElementById("mensaje").innerText = "Error de conexión";
+  }
+}
